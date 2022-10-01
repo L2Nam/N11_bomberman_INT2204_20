ef4f1614c49d56e8eef3beb2ec6c72d07d5009b9
L2Nam <lenamlhn234@gmail.com>
2022-10-01 17:06:57 +0700

    update_player

--- src/uet/oop/bomberman/Entity/Player.java
+++ src/uet/oop/bomberman/Entity/Player.java
@@ -1,5 +1,78 @@
 package uet.oop.bomberman.Entity;
 
-public class Player {
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
 
+public abstract class Player extends Entity {
+
+    protected boolean UpPressed = false;
+    protected boolean DownPressed = false;
+    protected boolean LeftPressed = false;
+    protected boolean RightPressed = false;
+    protected boolean ActionPressed = false;
+
+    /**
+     * Thuộc tính người chơi.
+     * @param position vị trí
+     * @param sprite hoạt ảnh
+     */
+    Player(Point2D.Float position, BufferedImage sprite) {
+        super(position, sprite);
+    }
+
+    public void toggleUpPressed() {
+        this.UpPressed = true;
+    }
+    public void toggleDownPressed() {
+        this.DownPressed = true;
+    }
+    public void toggleLeftPressed() {
+        this.LeftPressed = true;
+    }
+    public void toggleRightPressed() {
+        this.RightPressed = true;
+    }
+    public void toggleActionPressed() {
+        this.ActionPressed = true;
+    }
+
+    public void unToggleUpPressed() {
+        this.UpPressed = false;
+    }
+    public void unToggleDownPressed() {
+        this.DownPressed = false;
+    }
+    public void unToggleLeftPressed() {
+        this.LeftPressed = false;
+    }
+    public void unToggleRightPressed() {
+        this.RightPressed = false;
+    }
+    public void unToggleActionPressed() {
+        this.ActionPressed = false;
+    }
+
+    // sau làm thêm 1 hàm tạo lại keyMove dựa vào map
+    public void AutoMove() {
+        int keyMove = (int) (Math.random() * 3);
+        if (keyMove == 0) {
+            this.toggleDownPressed();
+        }
+        if (keyMove == 1) {
+            this.toggleUpPressed();
+        }
+        if (keyMove == 2) {
+            this.toggleLeftPressed();
+        }
+        if (keyMove == 3) {
+            this.toggleRightPressed();
+        }
+    }
+
+    public void UnMove() {
+        this.unToggleUpPressed();
+        this.unToggleDownPressed();
+        this.unToggleRightPressed();
+        this.unToggleLeftPressed();
+    }
 }
