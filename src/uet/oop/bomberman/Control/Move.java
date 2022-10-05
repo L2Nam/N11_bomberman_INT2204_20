829412269f42b64e2208cd60842086dc7a229e6b
L2Nam <lenamlhn234@gmail.com>
2022-10-05 21:38:19 +0700

    fix_move

--- src/uet/oop/bomberman/Control/Move.java
+++ src/uet/oop/bomberman/Control/Move.java
@@ -14,135 +14,135 @@ public class Move {
         }
     }
     //hiện thị hướng của các nhân vật
-    private static void setDirection(String direction, Character animal, int isMove) {
+    private static void setDirection(String direction, Character player, int isMove) {
         switch (direction) {
             case "down":
-                down_step(animal);
-                animal.setY(animal.getY() + isMove);
+                down_step(player);
+                player.setY(player.getY() + isMove);
                 break;
             case "up":
-                up_step(animal);
-                animal.setY(animal.getY() - isMove);
+                up_step(player);
+                player.setY(player.getY() - isMove);
                 break;
             case "left":
-                left_step(animal);
-                animal.setX(animal.getX() - isMove);
+                left_step(player);
+                player.setX(player.getX() - isMove);
                 break;
             case "right":
-                right_step(animal);
-                animal.setX(animal.getX() + isMove);
+                right_step(player);
+                player.setX(player.getX() + isMove);
                 break;
         }
     }
 
-    private static void down_step(Character animal) {      //hiện thị đi xuống
-        if (animal instanceof Player && animal.getY() % 8 == 0) {
-            if (animal.getSwap() == 1) {
-                animal.setImg(Sprite.control_down.getFxImage());
-                animal.setSwap(2);
-            } else if (animal.getSwap() == 2) {
-                animal.setImg(Sprite.control_down_1.getFxImage());
-                animal.setSwap(3);
-            } else if (animal.getSwap() == 3) {
-                animal.setImg(Sprite.control_down.getFxImage());
-                animal.setSwap(4);
+    private static void down_step(Character player) {      //hiện thị đi xuống
+        if (player instanceof Player && player.getY() % 8 == 0) {
+            if (player.getSwap() == 1) {
+                player.setImg(Sprite.control_down.getFxImage());
+                player.setSwap(2);
+            } else if (player.getSwap() == 2) {
+                player.setImg(Sprite.control_down_1.getFxImage());
+                player.setSwap(3);
+            } else if (player.getSwap() == 3) {
+                player.setImg(Sprite.control_down.getFxImage());
+                player.setSwap(4);
             } else {
-                animal.setImg(Sprite.control_down_2.getFxImage());
-                animal.setSwap(1);
+                player.setImg(Sprite.control_down_2.getFxImage());
+                player.setSwap(1);
             }
         }
     }
 
-    public static void down(Character animal) {        //điều khiển đi xuống
-        if (animal.getY() % 32 == 0 && animal.getX() % 32 == 0) {
-            if (animal instanceof Player && Blocked.block_down(animal)) {
-                animal.setDirection("down");
-                animal.setCount(4 / 1);
-                checkRun(animal);
+    public static void down(Character player) {        //điều khiển đi xuống
+        if (player.getY() % 32 == 0 && player.getX() % 32 == 0) {
+            if (player instanceof Player && Blocked.block_down(player)) {
+                player.setDirection("down");
+                player.setCount(4 / 1);
+                checkRun(player);
             }
         }
     }
 
-    public static void up(Character animal) {      //hiện thị đi lên
-        if (animal.getY() % 32 == 0 && animal.getX() % 32 == 0) {
-            if (animal instanceof Player && Blocked.block_up(animal)) {
-                animal.setDirection("up");
-                animal.setCount(4 / 1);
-                checkRun(animal);
+    public static void up(Character player) {      //hiện thị đi lên
+        if (player.getY() % 32 == 0 && player.getX() % 32 == 0) {
+            if (player instanceof Player && Blocked.block_up(player)) {
+                player.setDirection("up");
+                player.setCount(4 / 1);
+                checkRun(player);
             }
         }
     }
 
-    private static void up_step(Character animal) {        //cài đặt đi lên
-        if (animal instanceof Player && animal.getY() % 8 == 0) {
-            if (animal.getSwap() == 1) {
-                animal.setImg(Sprite.control_up.getFxImage());
-                animal.setSwap(2);
-            } else if (animal.getSwap() == 2) {
-                animal.setImg(Sprite.control_up_1.getFxImage());
-                animal.setSwap(3);
-            } else if (animal.getSwap() == 3) {
-                animal.setImg(Sprite.control_up.getFxImage());
-                animal.setSwap(4);
+    private static void up_step(Character player) {        //cài đặt đi lên
+        if (player instanceof Player && player.getY() % 8 == 0) {
+            if (player.getSwap() == 1) {
+                player.setImg(Sprite.control_up.getFxImage());
+                player.setSwap(2);
+            } else if (player.getSwap() == 2) {
+                player.setImg(Sprite.control_up_1.getFxImage());
+                player.setSwap(3);
+            } else if (player.getSwap() == 3) {
+                player.setImg(Sprite.control_up.getFxImage());
+                player.setSwap(4);
             } else {
-                animal.setImg(Sprite.control_up_2.getFxImage());
-                animal.setSwap(1);
+                player.setImg(Sprite.control_up_2.getFxImage());
+                player.setSwap(1);
             }
         }
     }
 
-    public static void left(Character animal) {        //hiện thị sang trái
-        if (animal.getX() % 32 == 0 && animal.getY() % 32 == 0) {
-            if (animal instanceof Player && Blocked.block_left(animal)) {
-                animal.setDirection("left");
-                animal.setCount(4 / 1);
-                checkRun(animal);
+    public static void left(Character player) {        //hiện thị sang trái
+        if (player.getX() % 32 == 0 && player.getY() % 32 == 0) {
+            if (player instanceof Player && Blocked.block_left(player)) {
+                player.setDirection("left");
+                player.setCount(4 / 1);
+                checkRun(player);
             }
         }
     }
 
-    private static void left_step(Character animal) {      //điều khiển sang trái
-        if (animal instanceof Player && animal.getX() % 8 == 0) {
-            if (animal.getSwap() == 1) {
-                animal.setImg(Sprite.control_left.getFxImage());
-                animal.setSwap(2);
-            } else if (animal.getSwap() == 2) {
-                animal.setImg(Sprite.control_left_1.getFxImage());
-                animal.setSwap(3);
-            } else if (animal.getSwap() == 3) {
-                animal.setImg(Sprite.control_left.getFxImage());
-                animal.setSwap(4);
+    private static void left_step(Character player) {      //điều khiển sang trái
+        if (player instanceof Player && player.getX() % 8 == 0) {
+            if (player.getSwap() == 1) {
+                player.setImg(Sprite.control_left.getFxImage());
+                player.setSwap(2);
+            } else if (player.getSwap() == 2) {
+                player.setImg(Sprite.control_left_1.getFxImage());
+                player.setSwap(3);
+            } else if (player.getSwap() == 3) {
+                player.setImg(Sprite.control_left.getFxImage());
+                player.setSwap(4);
             } else {
-                animal.setImg(Sprite.control_left_2.getFxImage());
-                animal.setSwap(1);
+                player.setImg(Sprite.control_left_2.getFxImage());
+                player.setSwap(1);
             }
         }
     }
 
-    public static void right(Character animal) {       //hiện thị sang phải
-        if (animal.getX() % 32 == 0 && animal.getY() % 32 == 0) {
-            if (animal instanceof Player && Blocked.block_right(animal)) {
-                animal.setDirection("right");
-                animal.setCount(4 / 1);
-                checkRun(animal);
+    public static void right(Character player) {       //hiện thị sang phải
+        if (player.getX() % 32 == 0 && player.getY() % 32 == 0) {
+            if (player instanceof Player && Blocked.block_right(player)) {
+                player.setDirection("right");
+                player.setCount(4 / 1);
+                checkRun(player);
             }
         }
     }
 
-    public static void right_step(Character animal) {      //điều khiển sang phải
-        if (animal instanceof Player && animal.getX() % 8 == 0) {
-            if (animal.getSwap() == 1) {
-                animal.setImg(Sprite.control_right.getFxImage());
-                animal.setSwap(2);
-            } else if (animal.getSwap() == 2) {
-                animal.setImg(Sprite.control_right_1.getFxImage());
-                animal.setSwap(3);
-            } else if (animal.getSwap() == 3) {
-                animal.setImg(Sprite.control_right.getFxImage());
-                animal.setSwap(4);
+    public static void right_step(Character player) {      //điều khiển sang phải
+        if (player instanceof Player && player.getX() % 8 == 0) {
+            if (player.getSwap() == 1) {
+                player.setImg(Sprite.control_right.getFxImage());
+                player.setSwap(2);
+            } else if (player.getSwap() == 2) {
+                player.setImg(Sprite.control_right_1.getFxImage());
+                player.setSwap(3);
+            } else if (player.getSwap() == 3) {
+                player.setImg(Sprite.control_right.getFxImage());
+                player.setSwap(4);
             } else {
-                animal.setImg(Sprite.control_right_2.getFxImage());
-                animal.setSwap(1);
+                player.setImg(Sprite.control_right_2.getFxImage());
+                player.setSwap(1);
             }
         }
     }
